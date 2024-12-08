0d13badbfbc9ac9ff6d9cc72032652ba476db6f6
Malberee <82259650+Malberee@users.noreply.github.com>
2024-12-08 16:26:45 +0200

    chore: install and configure eslint

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+// https://docs.expo.dev/guides/using-eslint/
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    '@feature-sliced/eslint-config/rules/public-api',
+    '@feature-sliced/eslint-config/rules/layers-slices',
+    'expo',
+    'prettier',
+  ],
+  plugins: ['prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'no-console': 'error',
+    '@typescript-eslint/no-unused-vars': 'error',
+    '@typescript-eslint/consistent-type-imports': [
+      'error',
+      {
+        prefer: 'type-imports',
+        fixStyle: 'inline-type-imports',
+      },
+    ],
+  },
+  ignorePatterns: ['/dist/*'],
+}
