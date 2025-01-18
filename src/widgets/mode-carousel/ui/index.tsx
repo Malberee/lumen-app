6d37ee4bfaf76feaea189509a6975cccf05f9e86
Malberee <82259650+Malberee@users.noreply.github.com>
2025-01-18 15:07:08 +0200

    feat: implement pagination

--- src/widgets/mode-carousel/ui/index.tsx
+++ src/widgets/mode-carousel/ui/index.tsx
@@ -1,6 +1,8 @@
+import { cssInterop } from 'nativewind'
 import { memo, useState } from 'react'
 import { Dimensions, View } from 'react-native'
-import Carousel from 'react-native-reanimated-carousel'
+import { useSharedValue } from 'react-native-reanimated'
+import Carousel, { Pagination } from 'react-native-reanimated-carousel'
 
 import { ColorPicker } from '@features/color-picker'
 
@@ -13,11 +15,17 @@ import {
 
 import { Mode } from './mode'
 
+const StyledPagination = cssInterop(Pagination.Basic, {
+  dotClassName: 'dotStyle',
+  activeDotClassName: 'activeDotStyle',
+})
+
 const width = Dimensions.get('window').width
 
 export const ModeCarousel = memo(() => {
   const [height, setHeight] = useState(0)
   const [showColorPicker, setShowColorPicker] = useState(false)
+  const progress = useSharedValue(0)
 
   const setMode = useModesStore((state) => state.setMode)
   const modes = useModesStore(selectAllModes)
@@ -27,27 +35,38 @@ export const ModeCarousel = memo(() => {
 
   return (
     <View className="flex-1 flex-row items-center">
-      <Carousel
-        data={modes}
-        ref={ref}
-        width={Math.round(width)}
-        height={Math.round(height)}
-        windowSize={3}
-        mode="parallax"
-        onScrollEnd={(index) => setMode(modes[index].name)}
-        modeConfig={{
-          parallaxScrollingScale: 1,
-          parallaxScrollingOffset: 50,
-          parallaxAdjacentItemScale: 0.9,
-        }}
-        renderItem={({ item }) => (
-          <Mode
-            mode={item}
-            showColorPicker={() => setShowColorPicker(true)}
-            onLayout={(e) => setHeight(e.nativeEvent.layout.height)}
-          />
-        )}
-      />
+      <View>
+        <Carousel
+          data={modes}
+          ref={ref}
+          width={Math.round(width)}
+          height={Math.round(height)}
+          windowSize={5}
+          mode="parallax"
+          onProgressChange={progress}
+          onScrollEnd={(index) => setMode(modes[index].name)}
+          modeConfig={{
+            parallaxScrollingScale: 1,
+            parallaxScrollingOffset: 50,
+            parallaxAdjacentItemScale: 0.9,
+          }}
+          renderItem={({ item }) => (
+            <Mode
+              mode={item}
+              showColorPicker={() => setShowColorPicker(true)}
+              onLayout={(e) => setHeight(e.nativeEvent.layout.height)}
+            />
+          )}
+        />
+        <StyledPagination
+          data={modes}
+          progress={progress}
+          dotClassName="bg-default-100 opacity-50 rounded-full "
+          activeDotClassName="rounded-full bg-default-200 "
+          containerStyle={{ gap: 4, marginTop: 8 }}
+        />
+      </View>
+
       {showColorPicker ? (
         <ColorPicker
           onClose={() => setShowColorPicker(false)}
