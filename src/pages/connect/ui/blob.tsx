9fb280d29832389e140a8e18a97f9ef249392396
Malberee <82259650+Malberee@users.noreply.github.com>
2025-05-03 15:18:49 +0300

    chore: replace svg blob with skia

--- src/pages/connect/ui/blob.tsx
+++ src/pages/connect/ui/blob.tsx
@@ -1,44 +1,53 @@
-import { cssInterop } from 'nativewind'
-import Svg, {
-  Defs,
-  FeGaussianBlur,
-  Filter,
-  G,
+import {
+  BlurMask,
+  Canvas,
   LinearGradient,
   Path,
-  Stop,
-} from 'react-native-svg'
-
-cssInterop(Svg, { className: 'style' })
+  Skia,
+  vec,
+} from '@shopify/react-native-skia'
+import { Dimensions } from 'react-native'
 
 export const Blob = () => {
+  const { width, height } = Dimensions.get('window')
+
+  const path = Skia.Path.MakeFromSVGString(
+    'M52.7,-39.4C67.5,-23.5,78.1,-1.6,73.4,16C68.8,33.7,49,47.2,28.3,56.2C7.5,65.2,-14.1,69.8,-31.2,62.4C-48.2,55.1,-60.6,35.9,-67.3,13.5C-73.9,-8.9,-74.7,-34.5,-62.5,-49.8C-50.3,-65.1,-25.1,-70.1,-3.1,-67.6C19,-65.2,37.9,-55.3,52.7,-39.4Z',
+  )!
+  const bounds = path.getBounds()
+
+  const canvasCenterX = width / 2
+  const canvasCenterY = height / 2
+
+  const shapeCenterX = bounds.x + bounds.width / 2
+  const shapeCenterY = bounds.y + bounds.height / 2
+
+  const dx = canvasCenterX - shapeCenterX
+  const dy = canvasCenterY - shapeCenterY
+
+  const transform = [
+    { translateX: dx },
+    { translateY: dy },
+    { scale: width / Math.max(bounds.width, bounds.height) },
+  ]
+
+  const gradient = (
+    <LinearGradient
+      start={vec(shapeCenterX, shapeCenterX - bounds.height / 2)}
+      end={vec(shapeCenterX, shapeCenterX + bounds.height / 2)}
+      colors={['#006FEE', '#2EC4FD']}
+    />
+  )
+
   return (
-    <Svg className="absolute size-[130%]" viewBox="0 0 200 200">
-      <Defs>
-        <LinearGradient id="gradient" x1="0" y1="0" x2="0" y2="1">
-          <Stop stopColor="#a931ff" />
-          <Stop offset="100%" stopColor="#2ec4fd" />
-        </LinearGradient>
-        <Filter id="blur" width="100" height="100">
-          <FeGaussianBlur stdDeviation="20" result="blur" />
-          <FeGaussianBlur stdDeviation="20" result="blur" />
-          <FeGaussianBlur stdDeviation="20" result="blur" />
-          <FeGaussianBlur stdDeviation="20" result="blur" />
-          <FeGaussianBlur stdDeviation="20" result="blur" />
-        </Filter>
-      </Defs>
-      <G transform="translate(100 100)">
-        <Path
-          fill="url(#gradient)"
-          d="M52.7,-39.4C67.5,-23.5,78.1,-1.6,73.4,16C68.8,33.7,49,47.2,28.3,56.2C7.5,65.2,-14.1,69.8,-31.2,62.4C-48.2,55.1,-60.6,35.9,-67.3,13.5C-73.9,-8.9,-74.7,-34.5,-62.5,-49.8C-50.3,-65.1,-25.1,-70.1,-3.1,-67.6C19,-65.2,37.9,-55.3,52.7,-39.4Z"
-        />
-        <Path
-          fill="url(#gradient)"
-          filter="url(#blur)"
-          opacity="0.8"
-          d="M52.7,-39.4C67.5,-23.5,78.1,-1.6,73.4,16C68.8,33.7,49,47.2,28.3,56.2C7.5,65.2,-14.1,69.8,-31.2,62.4C-48.2,55.1,-60.6,35.9,-67.3,13.5C-73.9,-8.9,-74.7,-34.5,-62.5,-49.8C-50.3,-65.1,-25.1,-70.1,-3.1,-67.6C19,-65.2,37.9,-55.3,52.7,-39.4Z"
-        />
-      </G>
-    </Svg>
+    <Canvas style={{ position: 'absolute', height, width }}>
+      <Path opacity={0.6} path={path} transform={transform}>
+        {gradient}
+        <BlurMask blur={70} />
+      </Path>
+      <Path path={path} transform={transform}>
+        {gradient}
+      </Path>
+    </Canvas>
   )
 }
