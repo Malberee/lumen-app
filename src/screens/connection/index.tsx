c7316280fd454727cb3f2ce2991d4877d2b6a5bb
Malberee <82259650+Malberee@users.noreply.github.com>
2025-11-07 22:19:16 +0200

    feat: implement connection page

---
+++ src/screens/connection/index.tsx
@@ -0,0 +1,52 @@
+import { router } from 'expo-router'
+import { Button, InfoIcon, Spinner } from 'merlo-ui'
+import { useCallback, useEffect, useState } from 'react'
+import { Text, View } from 'react-native'
+
+import { pingDevice } from './services'
+
+type ConnectionState = 'checking' | 'not-connected'
+
+export const Connection = () => {
+  const [connectionState, setConnectionState] =
+    useState<ConnectionState>('checking')
+
+  const checkConnection = useCallback(async () => {
+    setConnectionState('checking')
+    try {
+      await pingDevice()
+      router.replace('/ap')
+    } catch {
+      setConnectionState('not-connected')
+    }
+  }, [])
+
+  useEffect(() => {
+    checkConnection()
+  }, [checkConnection])
+
+  return (
+    <View className="flex-1 flex-row items-center justify-center">
+      {connectionState === 'checking' ? (
+        <Spinner
+          size="lg"
+          color="default"
+          label="Checking the connection to the device..."
+        />
+      ) : (
+        <View className="flex-col items-center">
+          <InfoIcon className="mb-6 text-warning" width={128} height={128} />
+          <Text className="text-large text-warning-500">
+            Unable to connect to the device
+          </Text>
+          <Text className="mb-6 text-large text-warning-700">
+            You may not be connected to an access point.
+          </Text>
+          <Button size="lg" color="warning" onPress={checkConnection}>
+            Try again
+          </Button>
+        </View>
+      )}
+    </View>
+  )
+}
