84805497e9b2b16d30b3d05cd043dad25913c607
Malberee <82259650+Malberee@users.noreply.github.com>
2025-11-10 19:56:51 +0200

    refactor(modes): move cards, header and led-ring to components

---
+++ src/screens/modes/components/index.ts
@@ -0,0 +1,3 @@
+export * from './cards'
+export * from './header'
+export * from './led-ring'
--- src/screens/modes/index.tsx
+++ src/screens/modes/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react'
 
-import { Cards } from './cards'
-import { Header } from './header'
+import { Cards, Header, LedRing } from './components'
 import { useUdpSync } from './hooks'
-import { LedRing } from './led-ring'
 
 export const Modes = () => {
   useUdpSync()
