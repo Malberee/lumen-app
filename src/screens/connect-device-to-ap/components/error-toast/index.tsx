8a3d0827653f1a0515e11ee4f3ce31054c336e4a
Malberee <82259650+Malberee@users.noreply.github.com>
2025-12-07 15:17:48 +0200

    chore: add custom danger color

--- src/screens/connect-device-to-ap/components/error-toast/index.tsx
+++ src/screens/connect-device-to-ap/components/error-toast/index.tsx
@@ -7,8 +7,8 @@ import { ShieldAlertIcon } from './shield-alert-icon'
 export const ErrorToast: FC<BaseToastProps> = ({ text1, text2 }) => {
   return (
     <View className="w-full px-4">
-      <View className="w-full flex-row items-center gap-4 rounded-medium border border-red-500 bg-surface p-4">
-        <ShieldAlertIcon className="h-8 w-8 text-red-500" />
+      <View className="w-full flex-row items-center gap-4 rounded-medium border border-danger bg-surface p-4">
+        <ShieldAlertIcon className="h-8 w-8 text-danger" />
         <View className="flex-1">
           <Text className="text-sm font-medium text-foreground">{text1}</Text>
           <Text className="text-sm text-foreground-400">{text2}</Text>
