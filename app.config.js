a2d91b2ba4b0e221d59e90564e95192ec693daa9
Malberee <82259650+Malberee@users.noreply.github.com>
2025-12-18 23:23:59 +0200

    build: install and configure expo-dev-properties

--- app.config.js
+++ app.config.js
@@ -1,54 +1,62 @@
-module.exports = () => ({
-  expo: {
-    name: 'Lumen',
-    slug: 'lumen-app',
-    scheme: 'com.malberee.lumen',
-    version: '1.0.0',
-    orientation: 'portrait',
-    icon: './assets/adaptive-icon.png',
-    userInterfaceStyle: 'light',
-    newArchEnabled: true,
-    android: {
-      adaptiveIcon: {
-        foregroundImage: './assets/adaptive-icon.png',
-        backgroundColor: '#09090B',
-      },
-      permissions: ['android.permission.ACCESS_FINE_LOCATION'],
-      package: 'com.malberee.lumen',
-    },
-    web: {
-      bundler: 'metro',
-      favicon: './assets/favicon.png',
-    },
-    plugins: [
-      [
-        'expo-router',
-        {
-          root: './src/app/routes',
-        },
-      ],
-      [
-        'expo-splash-screen',
-        {
+module.exports = () => {
+  const buildArchs =
+    process.env.BUILD_PROFILE === 'development'
+      ? ['arm64-v8a', 'x86_64']
+      : ['arm64-v8a']
+
+  return {
+    expo: {
+      name: 'Lumen',
+      slug: 'lumen-app',
+      scheme: 'com.malberee.lumen',
+      version: '1.0.0',
+      orientation: 'portrait',
+      icon: './assets/adaptive-icon.png',
+      userInterfaceStyle: 'light',
+      newArchEnabled: true,
+      android: {
+        adaptiveIcon: {
+          foregroundImage: './assets/adaptive-icon.png',
           backgroundColor: '#09090B',
-          image: './assets/splash-icon-light.png',
-          imageWidth: 200,
         },
+        permissions: ['android.permission.ACCESS_FINE_LOCATION'],
+        package: 'com.malberee.lumen',
+      },
+      web: {
+        bundler: 'metro',
+        favicon: './assets/favicon.png',
+      },
+      plugins: [
+        [
+          'expo-router',
+          {
+            root: './src/app/routes',
+          },
+        ],
+        [
+          'expo-splash-screen',
+          {
+            backgroundColor: '#09090B',
+            image: './assets/splash-icon-light.png',
+            imageWidth: 200,
+          },
+        ],
+        [
+          'expo-build-properties',
+          {
+            android: {
+              enableBundleCompression: true,
+              enableMinifyInReleaseBuilds: true,
+              buildArchs,
+            },
+          },
+        ],
       ],
-      [
-        './withAbiFilters',
-        {
-          abiFilters:
-            process.env.BUILD_PROFILE === 'development'
-              ? ['arm64-v8a', 'x86_64']
-              : ['arm64-v8a'],
+      extra: {
+        eas: {
+          projectId: 'b849292a-2223-4f58-aaf0-64e031539ff2',
         },
-      ],
-    ],
-    extra: {
-      eas: {
-        projectId: 'b849292a-2223-4f58-aaf0-64e031539ff2',
       },
     },
-  },
-})
+  }
+}
