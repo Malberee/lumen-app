042ca1f02b4724aa74434efd4f52e87f51c1b03e
Malberee <82259650+Malberee@users.noreply.github.com>
2025-12-27 16:56:04 +0200

    chore(layout): add SafeAreaProvider

--- src/app/routes/_layout.tsx
+++ src/app/routes/_layout.tsx
@@ -6,7 +6,10 @@ import { rem } from 'nativewind'
 import { View } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { configureReanimatedLogger } from 'react-native-reanimated'
-import { useSafeAreaInsets } from 'react-native-safe-area-context'
+import {
+  SafeAreaProvider,
+  useSafeAreaInsets,
+} from 'react-native-safe-area-context'
 
 import { colors } from '@constants'
 
@@ -19,32 +22,34 @@ const RootLayout = () => {
   const { top, bottom } = useSafeAreaInsets()
 
   return (
-    <GestureHandlerRootView>
-      <StatusBar style="light" translucent />
-      <View className="flex-1 dark">
-        <PortalProvider>
-          <Stack
-            screenOptions={{
-              headerShown: false,
-              contentStyle: {
-                backgroundColor: colors.background,
-                paddingTop: top,
-                paddingBottom: bottom,
-                paddingHorizontal: rem.get(),
-              },
-            }}
-          />
-        </PortalProvider>
-      </View>
-
-      {__DEV__ ? (
-        <View className="absolute left-1/2 top-12 -translate-x-1/2 flex-row gap-4">
-          <Button onPress={() => router.replace('/')}>Connect</Button>
-          <Button onPress={() => router.replace('/ap')}>Ap</Button>
-          <Button onPress={() => router.replace('/modes')}>Modes</Button>
+    <SafeAreaProvider>
+      <GestureHandlerRootView>
+        <StatusBar style="light" translucent />
+        <View className="flex-1 dark">
+          <PortalProvider>
+            <Stack
+              screenOptions={{
+                headerShown: false,
+                contentStyle: {
+                  backgroundColor: colors.background,
+                  paddingTop: top,
+                  paddingBottom: bottom,
+                  paddingHorizontal: rem.get(),
+                },
+              }}
+            />
+          </PortalProvider>
         </View>
-      ) : null}
-    </GestureHandlerRootView>
+
+        {__DEV__ ? (
+          <View className="absolute left-1/2 top-12 -translate-x-1/2 flex-row gap-4">
+            <Button onPress={() => router.replace('/')}>Connect</Button>
+            <Button onPress={() => router.replace('/ap')}>Ap</Button>
+            <Button onPress={() => router.replace('/modes')}>Modes</Button>
+          </View>
+        ) : null}
+      </GestureHandlerRootView>
+    </SafeAreaProvider>
   )
 }
 
